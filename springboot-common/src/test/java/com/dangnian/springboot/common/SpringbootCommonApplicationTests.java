56c4aff6df541ff5aa562ff48b881454ac170957
DangNian <2364352132@qq.com>
2020-04-24 01:27:05 +0800

    多module

---
+++ springboot-common/src/test/java/com/dangnian/springboot/common/SpringbootCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangnian.springboot.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springboot-dao/src/test/java/com/dangnian/springboot/dao/SpringbootDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangnian.springboot.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
