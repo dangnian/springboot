15caf8315e96b355113ac04191f4abe429a41a5a
chun.yin <2364352132@qq.com>
2020-04-26 14:31:14 +0800

    重新规划项目

---
+++ springboot-web/src/main/java/com/dangnian/springboot/web/config/SchedulingConfig.java
@@ -0,0 +1,37 @@
+package com.dangnian.springboot.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.util.ErrorHandler;
+
+@Configuration
+public class SchedulingConfig implements SchedulingConfigurer {
+
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setTaskScheduler(taskScheduler());
+    }
+
+    @Bean(name = "taskScheduler")
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("dispatch-");
+        scheduler.setAwaitTerminationSeconds(600);
+        scheduler.setErrorHandler(new MyErrorHandle());
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        return scheduler;
+    }
+
+    class MyErrorHandle implements ErrorHandler {
+
+        @Override
+        public void handleError(Throwable throwable) {
+
+        }
+    }
+}
---
+++ springboot-web/src/main/java/com/dangnian/springboot/web/testController/Controller.java
@@ -0,0 +1,12 @@
+package com.dangnian.springboot.web.testController;
+
+/**
+ * @ClassName Controller
+ * @Description TODO
+ * @Author chun.yin
+ * @Date 2020/4/26 14:05
+ * @Version 1.0
+ **/
+
+public class Controller {
+}
